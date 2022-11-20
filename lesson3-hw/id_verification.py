7ccb56ed0ef03164cca27a2bac3085d35247046d
zivattias <ziv.attias7@gmail.com>
2022-11-20 23:40:02 +0200

    beginning of ID generator

--- lesson3-hw/id_verification.py
+++ lesson3-hw/id_verification.py
@@ -18,8 +18,25 @@ def last_id_digit(id_num: str) -> str:
         last_digit: int = 10 - (digits_sum % 10)
     else:
         last_digit = 0
-    full_id = str(id_num) + str(last_digit)
+    full_id = "Your full ID number is: " + str(id_num) + str(last_digit)
     return full_id
+
+# The application:
+# Shows the instructions on how to use it.
+# The application will randomly fit the remaining numbers
+# 0 digits entered ⇒ application will generate all numbers
+# 1-8 digits entered by user ⇒ application will generate the remaining digits
+# More than 8 digits entered ⇒ application will reference only first 8
+
+# Program greeting message:
+print("Welcome to Ziv's ID generator. The program will generate a valid ID number according to your input.\n"
+      "If can enter how many digits you want, ranging from 0 to 8 digits.\n"
+      "Regardless of how many digits you entered - the program will complete the sequence to match valid Israeli ID.\n"
+      "Let's start!")
+# Initial user input:
+app_id: str = input('Enter your input: ')
+how_many_digits: int = len(app_id)
+
 
 if __name__ == '__main__':
     print(last_id_digit(input('Enter your ID number without last digit: ')))
