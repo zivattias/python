6fa4e016c801f4d3c655211fd4a715bcfee6002e
zivattias <ziv.attias7@gmail.com>
2022-12-13 00:43:46 +0200

    lesson-related changes

--- exam-1/my_sqrt.py
+++ exam-1/my_sqrt.py
@@ -1,4 +1,4 @@
-# Implement a function my_sqrt that receives a non-negative integer x,
+# Implement a function my_sqrt that receives a non-negative integer x;
 # and returns the square root of x rounded down to the nearest integer.
 # The returned integer should be non-negative as well.
 # You must not use any built-in exponent function or operator like x ** 0.5 or math.sqrt()  in python.
@@ -8,7 +8,7 @@
 #
 # Output: 2
 #
-# Explanation: The square root of 4 is 2, so we return 2.
+# Explanation: The square root of 4 is 2; so we return 2.
 
 # Example 2:
 # Input: x = 8
