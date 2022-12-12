e20071b157d36271ab64d85426a051436d2efd4a
zivattias <ziv.attias7@gmail.com>
2022-12-13 00:42:34 +0200

    textfile

---
+++ lesson10-hw/textfile/test_textfile.py
@@ -0,0 +1,54 @@
+import unittest
+from textfile import CsvFile, JsonFile, TxtFile, TextFile
+
+
+class TestTextfile(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.csv_1 = CsvFile('/Users/ziv.attias/PycharmProjects/python-course/lesson10-hw/textfile/files_ex/csv_1.csv')
+        cls.csv_2 = CsvFile('/Users/ziv.attias/PycharmProjects/python-course/lesson10-hw/textfile/files_ex/csv_2.csv',
+                            ';')
+        cls.csv_3 = CsvFile('/Users/ziv.attias/PycharmProjects/python-course/lesson10-hw/textfile/files_ex/csv_3.csv')
+
+        cls.txt_1 = TxtFile('/Users/ziv.attias/PycharmProjects/python-course/lesson10-hw/textfile/files_ex/txt_1.txt')
+        cls.txt_2 = TxtFile('/Users/ziv.attias/PycharmProjects/python-course/lesson10-hw/textfile/files_ex/txt_2.txt')
+
+        cls.json_1 = JsonFile(
+            '/Users/ziv.attias/PycharmProjects/python-course/lesson10-hw/textfile/files_ex/json_1.json')
+        cls.json_2 = JsonFile(
+            '/Users/ziv.attias/PycharmProjects/python-course/lesson10-hw/textfile/files_ex/json_2.json')
+
+    def test_paths(self):
+        self.assertTrue(self.csv_1)
+        self.assertTrue(self.txt_1)
+        self.assertTrue(self.json_1)
+        with self.assertRaises(Exception):
+            TextFile.__init__(self.csv_1, '/Users/username_or_email.csv')
+            TextFile.__init__(self.csv_1, '/Users/username_or_email.exe')
+
+    def test_extensions(self):
+        self.assertIs(CsvFile._get_extension(self.csv_1), 'csv')
+        self.assertIs(JsonFile._get_extension(self.json_1), 'json')
+        self.assertIs(TxtFile._get_extension(self.txt_1), 'txt')
+
+    def test_equal(self):
+        self.assertEqual(self.csv_1, self.csv_2)
+        self.assertEqual(self.txt_1, self.txt_2)
+        self.assertNotEqual(self.txt_1, self.json_1)
+
+    def test_add(self):
+        self.assertTrue(self.csv_1 + self.csv_2)
+        self.assertTrue(self.txt_1 + self.txt_2)
+        with self.assertRaises(TypeError, msg="Objects of different types cannot be concatenated"):
+            TextFile.__add__(self.csv_1, self.txt_1)
+
+    def test_new_file_dir(self):
+        self.assertEqual(CsvFile.get_combined_path(self.csv_1, self.csv_2),
+                         "/Users/ziv.attias/PycharmProjects/python-course/lesson10-hw/textfile/files_ex/csv_1_csv_2.csv")
+        self.assertEqual(TxtFile.get_combined_path(self.txt_1, self.txt_2),
+                         "/Users/ziv.attias/PycharmProjects/python-course/lesson10-hw/textfile/files_ex/txt_1_txt_2.txt")
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ lesson10-hw/textfile/textfile.py
@@ -0,0 +1,194 @@
+import csv
+import json
+import os
+from abc import ABC, abstractmethod
+
+
+class TextFile(ABC):
+    def __init__(self, fp: str):
+        if (not os.path.isfile(fp)) or os.path.splitext(fp)[-1][1:] != self._get_extension():
+            raise Exception()
+        self._fp = fp
+
+    @abstractmethod
+    def _get_content(self, fh):
+        pass
+
+    @abstractmethod
+    def _get_extension(self):
+        pass
+
+    @staticmethod
+    def get_combined_name(combined_fp: str) -> str:
+        return combined_fp.split('/')[-1]
+
+    def get_file_size(self):
+        return os.path.getsize(self._fp)
+
+    def get_file_path(self):
+        return self._fp
+
+    def return_content(self):
+        with open(self._fp, 'r') as fh:
+            content = self._get_content(fh)
+        return content
+
+    def get_combined_path(self, f2) -> str:
+        f1_path = self.get_file_path()
+        f2_path = f2.get_file_path()
+        f1_name = os.path.splitext(f1_path)[0].split('/')[-1]
+        f2_name = os.path.splitext(f2_path)[0].split('/')[-1]
+        return f"{'/'.join(f1_path.split('/')[:-1])}/" \
+               f"{f1_name}_{f2_name}.{self._get_extension()}"
+
+    def __eq__(self, other):
+        return type(self) == type(other)
+
+    def __add__(self, other):
+        if self != other:
+            raise TypeError("Objects of different types cannot be concatenated")
+
+        if isinstance(self, JsonFile) or isinstance(other, JsonFile):
+            raise TypeError('Concatenation not available for JSON files')
+
+        combined_path = self.get_combined_path(other)
+        if os.path.isfile(combined_path):
+            raise Exception("File already exists, can't overwrite")
+
+        self._concat(other)
+        return True
+
+    @abstractmethod
+    def _concat(self, other):
+        pass
+
+
+class CsvFile(TextFile):
+
+    def __init__(self, fp, delimiter=','):
+        super().__init__(fp)
+        self._delimiter = delimiter
+
+    def get_delimiter(self):
+        return self._delimiter
+
+    def _get_extension(self):
+        return 'csv'
+
+    def _get_content(self, fh):
+        ret_val = []
+        for row in csv.DictReader(fh, delimiter=self.get_delimiter()):
+            ret_val.append(row)
+        return ret_val
+
+    def get_rows_num(self) -> int:
+        rows = 0
+        with open(self.get_file_path(), 'r') as f:
+            reader = csv.reader(f, delimiter=self.get_delimiter())
+            for _ in reader:
+                rows += 1
+        return rows
+
+    def get_columns_num(self) -> int:
+        with open(self.get_file_path(), 'r') as f:
+            reader = csv.reader(f, delimiter=self.get_delimiter())
+            cols = len(next(reader))
+        return cols
+
+    def get_column(self, col_num: int) -> list:
+        with open(self.get_file_path(), 'r') as f:
+            reader = csv.reader(f, delimiter=self.get_delimiter())
+            if col_num + 1 > len(next(reader)):
+                raise IndexError('Column number is out of row range')
+            f.seek(0)
+            col_data = list()
+            for line in reader:
+                col_data.append(line[col_num])
+        return col_data
+
+    def get_cell(self, row_num, column_num):
+        with open(self.get_file_path(), 'r') as f:
+            reader = csv.reader(f, delimiter=self.get_delimiter())
+            if row_num > self.get_rows_num() or column_num > self.get_columns_num():
+                raise IndexError('Provided row/column out of range in file')
+            for i, row in enumerate(reader):
+                if i == row_num:
+                    cell = row[column_num]
+                    return cell
+
+    def get_row(self, row_num: int) -> dict:
+        with open(self.get_file_path(), 'r') as f:
+            if row_num <= 0:
+                raise ValueError("Row can't be zero (header) or negative")
+            if row_num + 1 > self.get_rows_num():
+                raise IndexError('Provided row out of range in file')
+            csv_test = f.read(1024)
+            f.seek(0)
+            reader = csv.DictReader(f, delimiter=self.get_delimiter())
+            for i, row in enumerate(reader):
+                if i + 1 == row_num:
+                    if csv.Sniffer().has_header(csv_test):
+                        return row
+                    ret_val = dict()
+                    for key, value in enumerate(row):
+                        ret_val[key + 1] = value
+                    return ret_val
+
+    def _concat(self, other):
+        with open(self.get_file_path(), 'r') as file1, open(other.get_file_path(), 'r') as file2:
+            reader1 = csv.DictReader(file1, delimiter=self.get_delimiter())
+            reader2 = csv.DictReader(file2, delimiter=other.get_delimiter())
+            headers1 = reader1.fieldnames
+            headers2 = reader2.fieldnames
+            if set(headers1) != set(headers2):
+                raise ValueError('CSV files must contain equivalent headers (order irrelevant)')
+            with open(self.get_combined_path(other), 'w') as new_f:
+                writer = csv.DictWriter(new_f, fieldnames=reader1.fieldnames)
+                writer.writeheader()
+                for row in reader1:
+                    writer.writerow(row)
+                for row in reader2:
+                    writer.writerow(row)
+                return True
+
+
+class TxtFile(TextFile):
+    def _get_extension(self):
+        return 'txt'
+
+    def _get_content(self, fh):
+        return fh.read()
+
+    def _concat(self, other):
+        merged_content = f"{self.return_content()} {other.return_content()}"
+        with open(self.get_combined_path(other), 'w') as new_f:
+            new_f.write(merged_content)
+
+    def get_words_num(self) -> int:
+        return len(self.return_content().split(' '))
+
+    def get_avg_word_len(self) -> float:
+        total_chars = len(''.join(self.return_content().split(' ')))
+        total_words = self.get_words_num()
+        return total_chars / total_words
+
+
+class JsonFile(TextFile):
+    def _get_extension(self):
+        return 'json'
+
+    def _get_content(self, fh):
+        return json.load(fh)
+
+    def _concat(self, other):
+        pass
+
+    def _load_json(self):
+        with open(self.get_file_path(), 'r') as f:
+            return json.load(f)
+
+    def is_list(self):
+        return True if type(self._load_json()) is list else False
+
+    def is_object(self):
+        return True if type(self._load_json()) is dict else False
