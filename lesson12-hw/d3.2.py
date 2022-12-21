b5537fe08631aee7df9fcd94d8ab0a1c11a5396d
zivattias <ziv.attias7@gmail.com>
2022-12-21 10:50:34 +0200

    comment typo

--- lesson12-hw/d3.2.py
+++ lesson12-hw/d3.2.py
@@ -1,6 +1,6 @@
 # Write generator batches(n, my_list) that returns batches of length n of the given list
 
-# when iterating it, the generator should yield sublists of length n from the original list my_list
+# when iterating it, the generator should yield sublist of length n from the original list my_list
 
 my_list = [[1, 2, 3], [1, 2], [4, 5, 6], [7, 8], [9], [10], [], ['a', 'b', 'c'], [True, False, None]]
 
