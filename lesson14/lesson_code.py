e75f92cd4c7765dc72cf9683d79831b8975a82f0
zivattias <ziv.attias7@gmail.com>
2022-12-25 12:25:25 +0200

    multiprocessing threads example

---
+++ lesson14/lesson_code.py
@@ -0,0 +1,23 @@
+import threading
+
+
+def a_lot_of_calcs(num: int):
+    print(f"Started {a_lot_of_calcs.__name__} with {num}")
+    s = 0
+    for i in range(10_000):
+        s += num ** i
+    print(f"\nFinished {a_lot_of_calcs.__name__} with {s}\n")
+
+
+if __name__ == '__main__':
+    print('Program start')
+
+    num1 = int(input('Enter your num: '))
+    t = threading.Thread(target = a_lot_of_calcs, args = [num1])
+    t.start()    # non-blocking action
+
+    num2 = int(input('Enter your num: '))
+    t1 = threading.Thread(target=a_lot_of_calcs, args=[num2])
+    t1.start()
+
+    print('Program finish')
