24c73b53bd0fd8b088ec26970646fd5a2a56ffc9
zivattias <ziv.attias7@gmail.com>
2023-01-04 09:01:15 +0200

    count binary substrings exercise

--- lesson15-hw/leetcode/count_binary.py
+++ lesson15-hw/leetcode/count_binary.py
@@ -2,3 +2,13 @@
 # and all the 0's and all the 1's in these substrings are grouped consecutively.
 #
 # Substrings that occur multiple times are counted the number of times they occur.
+# Input: s = "00110011"
+# Output: 6
+
+def count_binary(string: str):
+    s_lens = list(map(len, string.replace('01', '0 1').replace('10', '1 0').split()))
+    return sum(min(a, b) for a, b in zip(s_lens, s_lens[1:]))
+
+
+s = '0011001'
+print(count_binary(s))
