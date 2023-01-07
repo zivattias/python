62511e2a714a16a18f97c88b6eedad6217833066
zivattias <ziv.attias7@gmail.com>
2023-01-07 13:24:32 +0200

    skeletonized VT Class

--- lesson16-hw/VT_api_cmdline/backend.py
+++ lesson16-hw/VT_api_cmdline/backend.py
@@ -1,9 +1,44 @@
 # WIP
+import base64
+import os
+import argparse
+from concurrent.futures import ThreadPoolExecutor
+from threading import Lock
+from functools import lru_cache
+
+'''
+VirusTotal API responses:
+
+URL analysis:
+1. 404 for a never-scanned URL
+2. 200 for a scanned URL, returns an analysis dict:
+Result stats - ["data"]["attributes"]["last_analysis_stats"] (dict['harmless', 'malicious', 'suspicious', 'undetected'])
+Last submission date - ["data"]["attributes"]["last_submission_date"] (epoch timestamp)
+
+URL scan:
+1. 400 for an invalid URL - "Unable to canonicalize url"
+2. 200 for success, returns dict["data"] = {"type": "analysis, "id": str}
+'''
+
 
 class VTAnalyzer:
 
     def __init__(self):
-        self._cache = dict()
+        # Cache maps URL Base64 strings to a respective (last_analysis_time, (result, ratio)) nested tuple
+        self._cache: dict[str: tuple[str, tuple[str, float]]] = dict()
+        # A uniformed Lock() for threaded actions, e.g. accessing cache and executing actions on it
+        self._lock = Lock()
+        # Argparse utilization
+        self._parser = argparse.ArgumentParser(prog="URL Reputation Check, Powered by VirusTotal's API",
+                                               description="The program allows you to check URL(s)",
+                                               epilog="By Ziv Attias")
+        self._parser.add_argument('url', help='URL to scan')
+        self._parser.add_argument('-k', '--apikey')
+        self._parser.add_argument('-s', '--scan', action='store_true')
+
+    @staticmethod
+    def base_encoder(url: str):
+        return base64.urlsafe_b64encode(f"{url}".encode()).decode().strip("=")
 
     def get_urls_reputation(self, urls: list) -> dict[str, bool]:
         pass
