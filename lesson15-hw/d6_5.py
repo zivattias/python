8eddaf73603a9ffaeeac2a37c55175c18289874f
zivattias <ziv.attias7@gmail.com>
2023-01-07 12:25:51 +0200

    added bonus JSON creation

--- lesson15-hw/d6_5.py
+++ lesson15-hw/d6_5.py
@@ -2,10 +2,14 @@
 # and creates a nested dictionary that corresponds to the structure of the directory and the files in it.
 
 import os
+import json
 from pprint import pprint
 
 
-def dir2dict(dp: str):
+def dir2dict(dp: str, jp: str = None):
+    if not os.path.exists(dp):
+        raise FileNotFoundError(f"Dir {dp} not found")
+
     directory_dict = {}
 
     for root, dirs, files in os.walk(dp):
@@ -13,7 +17,14 @@ def dir2dict(dp: str):
         directory_dict[os.path.basename(root)] = dir_dict
         break
 
+    if jp:
+        with open(jp, 'w') as f:
+            json.dump(directory_dict, f)
+            print('JSON file created')
+
     return directory_dict
 
 
+pprint(dir2dict('dir_ex', './files/d6_5.json'))
+print('\n')
 pprint(dir2dict('dir_ex'))
