5192656aa75670e1f3ab261af60cc435340bbd6f
zivattias <ziv.attias7@gmail.com>
2023-01-09 19:45:35 +0200

    optimized code

--- lesson16-hw/VT_api_cmdline/backend.py
+++ lesson16-hw/VT_api_cmdline/backend.py
@@ -33,7 +33,7 @@ class VTAnalyzer:
     2. 200 for success, returns dict["data"] = {"type": "analysis, "id": str}
     """
 
-    def __init__(self, urls: list[str], apikey: str, scan: bool, quota: bool, verbose: bool, age: int):
+    def __init__(self, urls: list[str], apikey: str, scan: bool, quota: bool, verbose: bool, age: str):
         self._urls = urls
         self._scan = scan
         self._quota = quota
@@ -41,7 +41,7 @@ class VTAnalyzer:
         # API key init
         self._token = apikey if apikey else os.environ["VT_KEY"]
         # Cache age
-        self._cache_age = age
+        self._cache_age = int(age)
 
         # Cache maps URL strings to a respective (last_analysis_date, (result, ratio)) nested tuple
         if not os.path.exists('cache.json'):
@@ -70,13 +70,14 @@ class VTAnalyzer:
         :return bool: if today's date - last analysis date <= self._cache_age
         """
         datetime_epoch = datetime.utcfromtimestamp(epoch)
-        if self._verbose:
+
+        if datetime.utcnow() - datetime_epoch <= timedelta(days=self._cache_age):
             print(f"Found valid cached data for URL: {url}. "
-                  f"Last analysis date: {datetime_epoch.date().strftime('%d-%m-%Y')}")
+                  f"Last analysis date: {datetime_epoch.date().strftime('%d-%m-%Y')}") if self._verbose else None
+            return True
+        return False
 
-        return True if datetime.utcnow() - datetime_epoch <= timedelta(days=self._cache_age) else False
-
-    def scan_url(self, url: str) -> bool:
+    def scan_url(self, url: str) -> tuple[bool, str]:
         """
         Scans URL using VirusTotal API
         :param url: URL string
@@ -90,15 +91,13 @@ class VTAnalyzer:
             "content-type": "application/x-www-form-urlencoded"
         }
 
-        if self._verbose:
-            print(f"Scanning URL {url}...")
+        print(f"Scanning URL {url}...") if self._verbose else None
 
         response = requests.post(SCAN_URL, data=data, headers=headers)
 
         if response.status_code == 200:
-            if self._verbose:
-                print(f"URL has been successfully scanned!")
-            return True
+            print(f"URL {url} has been successfully scanned!") if self._verbose else None
+            return True, url
         raise BadRequest(request=response, status_code=response.status_code)
 
     @staticmethod
@@ -110,8 +109,7 @@ class VTAnalyzer:
         ratio = accuracy percentage
         """
         stats = json_resp['data']['attributes']['last_analysis_stats']
-        total_values_sum = sum(stats.values())
-        max_val = max(stats.values())
+        total_values_sum, max_val = sum(stats.values()), max(stats.values())
         max_key = list(stats.keys())[list(stats.values()).index(max_val)]
         ratio = max_val / total_values_sum * 100
         return max_key, ratio
@@ -124,34 +122,42 @@ class VTAnalyzer:
         If the URL hasn't been scanned before, the method will perform a scan and re-analyze
         Finally, it will store the data in self._cache accordingly
         """
+        # In case of force-scanning the input for this func is a Future obj, thus extracting its result
+        # Which is tuple[bool, url: str]
+        # if not isinstance(url, str):
+        #     url = url.result()[1]
+
         full_url = f"{ANALYSIS_URL}/{self.encode_url(url)}"
         headers = {
             "accept": "application/json",
             "x-apikey": self._token
         }
 
+        print(f"Analyzing URL {url}...") if self._verbose else None
+
         response = requests.get(url=full_url, headers=headers)
 
         if response.status_code == 404:
-            if self._verbose:
-                print(f"Found no analysis for URL {url}, proceeding to scan")
-            if self.scan_url(url):
-                self.analyze_url(url)
+            print(f"Found no analysis for URL {url}, initializing scan") if self._verbose else None
+            self.analyze_url(url) if self.scan_url(url)[0] else None
 
         if response.status_code == 200 or response.status_code == 202:
             json_resp = response.json()
-            last_analysis_date = json_resp["data"]["attributes"]["last_analysis_date"]
+            last_analysis_epoch = json_resp["data"]["attributes"]["last_analysis_date"]
             max_key, ratio = self._get_url_reputation(json_resp)
 
-            self._cache[url] = (last_analysis_date, (max_key, ratio))
-
-        return self._cache[url]
+            with self._lock:
+                self._cache[url] = (last_analysis_epoch, (max_key, ratio))
 
     def _single_url_flow(self, url: str) -> list:
-        if url not in self._cache or not self.check_cache(self._cache[url][0], url):
-            cache_data = self.analyze_url(url)
-            return [url, cache_data[0], cache_data[1][0], cache_data[1][1], 'api']
-        return [url, self._cache[url][0], self._cache[url][1][0], self._cache[url][1][1], 'cache']
+        source = 'cache'
+
+        if (url not in self._cache) or ((url in self._cache) and (not self.check_cache(self._cache[url][0], url))):
+            print(f"Either URL {url} not in cache or cache is outdated, proceeding to analysis")
+            self.analyze_url(url)
+            source = 'api'
+
+        return [url, self._cache[url][0], self._cache[url][1][0], self._cache[url][1][1], source]
 
     def main(self):
         ret_val = list()
@@ -159,7 +165,8 @@ class VTAnalyzer:
         if isinstance(self._urls, list):
             with ThreadPoolExecutor() as executor:
                 for url in self._urls:
-                    executor.submit(self.scan_url, url) if self._scan else None
+                    if self._scan:
+                        executor.submit(self.scan_url, url)
                     ret_val.append(executor.submit(self._single_url_flow, url).result())
 
         # If user input a single URL:
