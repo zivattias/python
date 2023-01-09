cae7999d4ca7644e7ed893090be2b9f70878cb5a
zivattias <ziv.attias7@gmail.com>
2023-01-09 15:48:24 +0200

    VirusTotal API Terminal program

--- lesson16-hw/VT_api_cmdline/backend.py
+++ lesson16-hw/VT_api_cmdline/backend.py
@@ -2,7 +2,6 @@ import os
 import json
 import base64
 import requests
-import argparse
 from consts import *
 from exceptions import *
 from threading import Lock
@@ -25,7 +24,8 @@ class VTAnalyzer:
     URL analysis:
     1. 404 for a never-scanned URL
     2. 200 for a scanned URL, returns an analysis dict:
-    Result stats - ["data"]["attributes"]["last_analysis_stats"] (dict['harmless', 'malicious', 'suspicious', 'undetected'])
+    Result stats - ["data"]["attributes"]["last_analysis_stats"]
+    (dict['harmless', 'malicious', 'suspicious', 'undetected'])
     Last analysis date - ["data"]["attributes"]["last_analysis_date"] (epoch timestamp)
 
     URL scan:
@@ -33,37 +33,25 @@ class VTAnalyzer:
     2. 200 for success, returns dict["data"] = {"type": "analysis, "id": str}
     """
 
-    def __init__(self):
+    def __init__(self, urls: list[str], apikey: str, scan: bool, quota: bool, verbose: bool, age: int):
+        self._urls = urls
+        self._scan = scan
+        self._quota = quota
+        self._verbose = verbose
+        # API key init
+        self._token = apikey if apikey else os.environ["VT_KEY"]
+        # Cache age
+        self._cache_age = age
+
         # Cache maps URL strings to a respective (last_analysis_date, (result, ratio)) nested tuple
-        mode = 'w' if not os.path.exists('cache.json') else 'r'
-        with open('cache.json', mode=mode) as f:
-            self._cache: dict[int: tuple[str, tuple[str, float]]] = json.load(f)
+        if not os.path.exists('cache.json'):
+            self._cache = dict()
+        else:
+            with open('cache.json', mode='r') as f:
+                self._cache = json.load(f)
+
         # Uniformed Lock() for threaded actions, e.g. accessing cache and executing actions on it
         self._lock = Lock()
-        # Argparse initialization
-        self._parser = argparse.ArgumentParser(prog="URL Reputation Check, Powered by VirusTotal's API",
-                                               description="The program allows you to check URL(s)",
-                                               epilog="By Ziv Attias")
-        self._parser.add_argument('-u', '--url', nargs='*',
-                                  help='followed by one or more URLs, separated by a whitespace')
-        self._parser.add_argument('-k', '--apikey',
-                                  help='followed by custom VT API key')
-        self._parser.add_argument('-s', '--scan', action='store_true',
-                                  help='force URL scan')
-        self._parser.add_argument('-q', '--quota', action='store_true',
-                                  help='verbose wait in case of quota insufficiency')
-        self._parser.add_argument('-v', '--verbose', action='store_true',
-                                  help='verbose prints throughout the process')
-        self._parser.add_argument('-a', '--age', default=182,
-                                  help='declare cache max age (days), default = 182')
-        self._args = self._parser.parse_args()
-        # API key init
-        if self._args.apikey:
-            self._token = self._args.apikey
-        else:
-            self._token = os.environ["VT_KEY"]
-        # Cache age
-        self._cache_age = self._args.age
 
     @staticmethod
     def encode_url(url: str):
@@ -74,16 +62,17 @@ class VTAnalyzer:
         """
         return base64.urlsafe_b64encode(f"{url}".encode()).decode().strip("=")
 
-    def check_cache(self, epoch: int) -> bool:
+    def check_cache(self, epoch: int, url: str) -> bool:
         """
         Check cached content age
-        :param epoch: str
+        :param url: URL str
+        :param epoch: Unix timestamp int
         :return bool: if today's date - last analysis date <= self._cache_age
         """
         datetime_epoch = datetime.utcfromtimestamp(epoch)
-
-        if self._args.verbose:
-            print(f"Last analysis date: {datetime_epoch.date()}")
+        if self._verbose:
+            print(f"Found valid cached data for URL: {url}. "
+                  f"Last analysis date: {datetime_epoch.date().strftime('%d-%m-%Y')}")
 
         return True if datetime.utcnow() - datetime_epoch <= timedelta(days=self._cache_age) else False
 
@@ -100,13 +89,14 @@ class VTAnalyzer:
             "x-apikey": self._token,
             "content-type": "application/x-www-form-urlencoded"
         }
-        if self._args.verbose:
+
+        if self._verbose:
             print(f"Scanning URL {url}...")
 
         response = requests.post(SCAN_URL, data=data, headers=headers)
 
         if response.status_code == 200:
-            if self._args.verbose:
+            if self._verbose:
                 print(f"URL has been successfully scanned!")
             return True
         raise BadRequest(request=response, status_code=response.status_code)
@@ -134,15 +124,16 @@ class VTAnalyzer:
         If the URL hasn't been scanned before, the method will perform a scan and re-analyze
         Finally, it will store the data in self._cache accordingly
         """
-        url = f"{ANALYSIS_URL}/{self.encode_url(url)}"
+        full_url = f"{ANALYSIS_URL}/{self.encode_url(url)}"
         headers = {
             "accept": "application/json",
             "x-apikey": self._token
         }
 
-        response = requests.get(url=url, headers=headers)
+        response = requests.get(url=full_url, headers=headers)
+
         if response.status_code == 404:
-            if self._args.verbose:
+            if self._verbose:
                 print(f"Found no analysis for URL {url}, proceeding to scan")
             if self.scan_url(url):
                 self.analyze_url(url)
@@ -154,23 +145,33 @@ class VTAnalyzer:
 
             self._cache[url] = (last_analysis_date, (max_key, ratio))
 
+        return self._cache[url]
+
+    def _single_url_flow(self, url: str) -> list:
+        if url not in self._cache or not self.check_cache(self._cache[url][0], url):
+            cache_data = self.analyze_url(url)
+            return [url, cache_data[0], cache_data[1][0], cache_data[1][1], 'api']
+        return [url, self._cache[url][0], self._cache[url][1][0], self._cache[url][1][1], 'cache']
+
     def main(self):
-        args = self._args
+        ret_val = list()
+        # If user input a list of URLs:
+        if isinstance(self._urls, list):
+            with ThreadPoolExecutor() as executor:
+                for url in self._urls:
+                    executor.submit(self.scan_url, url) if self._scan else None
+                    ret_val.append(executor.submit(self._single_url_flow, url).result())
 
-        if isinstance(args.urls, list):
-            for url in args.urls:
-                pass
+        # If user input a single URL:
+        if isinstance(self._urls, str):
+            self.scan_url(self._urls) if self._scan else None
+            ret_val.append(self._single_url_flow(self._urls))
 
-        if isinstance(args.urls, str):
-            pass
+        # Pretty result prints:
+        for result in ret_val:
+            print(f"URL: {result[0]}, analysis date: {datetime.utcfromtimestamp(result[1]).strftime('%d-%m-%Y')},"
+                  f" result: {result[2]}, accuracy: {result[3]:.2f}%, source: {result[4]}")
 
-        if url in self._cache:
-            pass
-        # check if last_analysis_date stands with cache policy
-        # if it does, return result
-        # if it doesn't, re-analyze
-        if url not in self._cache:
-            pass
-        # analyze url
-        # if analysis status code is 404 > scan > re-analyze
-        # store url data in self._cache
+        # Save cache:
+        with open('cache.json', 'w') as f:
+            json.dump(self._cache, f)
--- lesson16-hw/VT_api_cmdline/main.py
+++ lesson16-hw/VT_api_cmdline/main.py
@@ -1,11 +1,26 @@
 from backend import VTAnalyzer
+import argparse
 
 if __name__ == '__main__':
-    # VTAnalyzer initialization
-    analyzer = VTAnalyzer()
-    # Run main method
+    # Argparse initialization
+    parser = argparse.ArgumentParser(prog="URL Reputation Check, Powered by VirusTotal's API",
+                                     description="The program allows you to check URL(s)",
+                                     epilog="By Ziv Attias")
+    parser.add_argument('url', nargs='*',
+                        help='one or more URLs, separated by a whitespace')
+    parser.add_argument('-k', '--apikey',
+                        help='followed by custom VT API key')
+    parser.add_argument('-s', '--scan', action='store_true',
+                        help='force URL scan')
+    parser.add_argument('-q', '--quota', action='store_true',
+                        help='verbose wait in case of quota insufficiency')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='verbose prints throughout the process')
+    parser.add_argument('-a', '--age', default=182,
+                        help='declare cache max age (days), default = 182')
+    args = parser.parse_args(['https://api.kanye.rest', 'https://google.com/', '-v', '-s'])
+
+    # Create an instance of VTAnalyzer with the parsed arguments and run main()
+    analyzer = VTAnalyzer(urls=args.url, apikey=args.apikey, scan=args.scan,
+                          quota=args.quota, verbose=args.verbose, age=args.age)
     analyzer.main()
-
-
-
-
