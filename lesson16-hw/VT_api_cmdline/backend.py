fcb810f5a34f9e384fae1e52952e45a02a7918af
zivattias <ziv.attias7@gmail.com>
2023-01-10 12:11:54 +0200

    extra invalid URL(s) handling coverage

--- lesson16-hw/VT_api_cmdline/backend.py
+++ lesson16-hw/VT_api_cmdline/backend.py
@@ -122,11 +122,6 @@ class VTAnalyzer:
         If the URL hasn't been scanned before, the method will perform a scan and re-analyze
         Finally, it will store the data in self._cache accordingly
         """
-        # In case of force-scanning the input for this func is a Future obj, thus extracting its result
-        # Which is tuple[bool, url: str]
-        # if not isinstance(url, str):
-        #     url = url.result()[1]
-
         full_url = f"{ANALYSIS_URL}/{self.encode_url(url)}"
         headers = {
             "accept": "application/json",
@@ -138,11 +133,8 @@ class VTAnalyzer:
         response = requests.get(url=full_url, headers=headers)
 
         if response.status_code == 404:
-            print(f"Found no analysis for URL {url}, initializing scan") if self._verbose else None
-            if self.scan_url(url)[0]:
-                time.sleep(2)
-
-        response = requests.get(url=full_url, headers=headers)
+            print(f"Found no analysis for URL {url}, perhaps use --scan / -s to scan first?") if self._verbose else None
+            return False
 
         if response.status_code == 200 or response.status_code == 202:
             print(f"Analyzed URL {url} successfully!")
@@ -151,29 +143,40 @@ class VTAnalyzer:
             max_key, ratio = self._get_url_reputation(json_resp)
 
             with self._lock:
-                self._cache[url] = (last_analysis_epoch, (max_key, ratio))
+                self._cache[url] = [last_analysis_epoch, (max_key, ratio)]
+                return last_analysis_epoch, (max_key, ratio)
 
-            return self._cache[url]
-
-    def _single_url_flow(self, url: str) -> list:
+    def _single_url_flow(self, url: str) -> list | bool:
         source = 'cache'
 
         if (url not in self._cache) or ((url in self._cache) and (not self.check_cache(self._cache[url][0], url))):
             print(f"Either URL {url} not in cache or cache is outdated, proceeding to analysis")
             ret_val = self.analyze_url(url)
+            if ret_val is False:
+                return False
+
             source = 'api'
             # If VirusTotal db returns an outdated analysis, inform user the program re-scans & re-analyzes
             days_since = datetime.utcnow() - datetime.utcfromtimestamp(ret_val[0])
+
             if days_since > timedelta(days=self._cache_age):
                 print(f"URL {url} last analysis ({ret_val[1][0]}, {ret_val[1][1]:.2f}%) took place on: "
                       f"{datetime.utcfromtimestamp(ret_val[0]).strftime('%d-%m-%Y')}, "
                       f"{days_since.days} days ago. "
                       f"Re-scanning to uphold with cache age limit: {self._cache_age} days") if self._verbose else None
                 self.scan_url(url)
-                print("Waiting 2 seconds...")
-                time.sleep(2)
-                ret_val = self.analyze_url(url)
-                return [url, ret_val[0], ret_val[1][0], ret_val[1][1], source]
+
+                full_url = f"{ANALYSIS_URL}/{self.encode_url(url)}"
+                headers = {
+                    "accept": "application/json",
+                    "x-apikey": self._token
+                }
+                response = requests.get(url=full_url, headers=headers)
+                json_resp = response.json()
+                last_analysis_epoch = json_resp["data"]["attributes"]["last_analysis_date"]
+                max_key, ratio = self._get_url_reputation(json_resp)
+                self._cache[url] = last_analysis_epoch, (max_key, ratio)
+                return [url, last_analysis_epoch, max_key, ratio, source]
 
         return [url, self._cache[url][0], self._cache[url][1][0], self._cache[url][1][1], source]
 
@@ -191,9 +194,12 @@ class VTAnalyzer:
                     ret_val.append(executor.submit(self._single_url_flow, url).result())
 
         # Pretty result prints:
-        for result in ret_val:
-            print(f"URL: {result[0]}, analysis date: {datetime.utcfromtimestamp(result[1]).strftime('%d-%m-%Y')},"
-                  f" result: {result[2]}, accuracy: {result[3]:.2f}%, source: {result[4]}")
+        for i, result in enumerate(ret_val):
+            if result is not False:
+                print(f"URL: {result[0]}, analysis date: {datetime.utcfromtimestamp(result[1]).strftime('%d-%m-%Y')},"
+                      f" result: {result[2]}, accuracy: {result[3]:.2f}%, source: {result[4]}")
+            else:
+                print(f"URL {i} result: N/A")
 
         # Save cache:
         with open('cache.json', 'w') as f:
